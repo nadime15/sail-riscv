b37f4cfa46bad9f230302437c0b902d2dbee8ee5
Prashanth Mundkur <pmundkur.riscv@gmail.com>
2025-10-13 07:49:50 -0500

    Remove EAGAIN error handling since the listening socket is now blocking. (#10)
    * Remove EAGAIN error handling since the listening socket is now blocking.
    
    * Better handling of OpenOCD disconnects.

--- c_emulator/remote_bitbang.cpp
+++ c_emulator/remote_bitbang.cpp
@@ -138,13 +138,9 @@ void remote_bitbang_t::accept_connection()
 {
   client_fd = accept(socket_fd, NULL, NULL);
   if (client_fd == -1) {
-    if (errno == EAGAIN) {
-      // No client waiting to connect right now.
-    } else {
-      fprintf(stderr, "failed to accept on socket: %s (%d)\n", strerror(errno),
-              errno);
-      exit(EXIT_FAILURE);
-    }
+    fprintf(stderr, "failed to accept on socket: %s (%d)\n", strerror(errno),
+            errno);
+    exit(EXIT_FAILURE);
   } else {
     fcntl(client_fd, F_SETFL, O_NONBLOCK);
   }
@@ -283,7 +279,9 @@ void remote_bitbang_t::run(uint64_t insn_limit)
     {
       int max_ticks = 100;
       int ticks = 0;
-      while (ticks < max_ticks) {
+      // Since OpenOCD could disconnect anytime in the loop,
+      // ensure the socket is valid before calling `tick()`.
+      while (ticks < max_ticks && client_fd > 0) {
         // By tracking whether a request was sent from OpenOCD, we can avoid
         // timing issues (request timing out due no responses) and no longer
         // need to tweak 'max_ticks'. As soon as a new request is detected,
